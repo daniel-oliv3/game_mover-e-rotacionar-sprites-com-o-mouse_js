138685f974c657cb1a7ebdb0b861b7fbc1e555cc
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-01-07 15:57:01 -0300

    Update

--- js/script.js
+++ js/script.js
@@ -1,8 +1,29 @@
 var cnv = document.querySelector('canvas');
 var ctx = cnv.getContext('2d');
 
-function loop(){
+var img = new Image();
+img.src = "img/aviao.png";
 
+var objs = {
+    x: 100,
+    y: 100,
+    width: 800,
+    height: 64,
+    rotation: 0,
+    img: img,
+    centerX: function(){
+        return this.x + this.width/2;
+    },
+    centerY: function(){
+        return this.y + this.height/2;
+    }
+}
+
+function loop(){
+    requestAnimationFrame(loop, cnv){
+        update();
+        render();
+    }
 }
 
 function update(){
